c41d00a03fe216a96a980a1cc0e3b9c1ed73a7cb
jongwook98 <oucsa1541@naver.com>
2022-03-21 22:30:05 +0900

    total GCD

--- 2.수학/조합0의개수.py
+++ 2.수학/조합0의개수.py
@@ -1,19 +1,52 @@
 from sys import stdin
 
+# 구현은 성공했으나 시간초과....
+# 핵심은 2와 5의 소인수를 구하는 알고리즘의 시간을 줄이는 방법인듯
+# python 의 // 연산은 소수 부분은 버리고 정수 부분만 추출
+
 N, M = map(int, stdin.readline().strip().split())
 
-num_of_plus_zero = 0
-num_of_minus_zero = 0
+def two_count(n):
+    two = 0
+    while n != 0:
+        n = n // 2
+        two += n
+    return two
 
-for i in range(N+1, 5):
+def five_count(n):
+    five = 0
+    while n != 0:
+        n = n // 5
+        five += n
+    return five
+
+
+num_of_two = two_count(N) - two_count(M) - two_count(N-M)
+num_of_five = five_count(N) - five_count(M) - five_count(N-M)
+
+print(min(num_of_two, num_of_five))
+''' 내 알고리즘...
+N, M = map(int, stdin.readline().strip().split())
+
+num_of_two = 0
+num_of_five = 0
+
+for i in range(1, N+1):
+    count = 1
+    if i <= M:
+        count -= 1
+    if i <= (N-M):
+        count -= 1
+
     num = i
+    while num != 0:
+        num_of_two += count
+        num = num // 2
 
-    if num % 5 == 0:
-        if i <= M:
-            num_of_minus_zero += 1
-        if i <= (N-M):
-            num_of_minus_zero += 1
-        num_of_plus_zero += 1
-        num /= 5
+    num = i
+    while num != 0:
+        num_of_five += count
+        num = num // 5
 
-print(num_of_plus_zero - num_of_minus_zero)
+print(min(num_of_two, num_of_five))
+'''
